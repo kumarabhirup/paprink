50b66e80dd0db27721fb0d470ea8055fe4bef65c
Kumar Abhirup <kumar@iqubex.com>
2019-02-26 20:29:40 +0530

    👌🏻 IMPROVE: Add card black background before image render

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ export default class Card extends Component {
     const { post } = this.props
     return (
       <div className={`card ${ this.props.type === 'small_image' && 'card_small_with_image' } ${ this.props.type === 'small_background' && 'card_default card_small_with_background' } ${ this.props.type === 'largest' && 'card_largest_with_image' } ${ this.props.type === 'mini' && 'card_default card_default_no_image' } grid-item`}>
-        { this.props.type === 'small_background' && <div className="card_background" style={{backgroundImage:`url(${post.thumbnail})`}}></div> }
+        { this.props.type === 'small_background' && <div className="card_background" style={{backgroundColor: "black", backgroundImage:`url(${post.thumbnail})`}}></div> }
         { this.props.type != 'small_background' && this.props.type != 'mini' && <img className="card-img-top" src={post.thumbnail} alt={post.title} /> }
         <div className="card-body">
           <div className={`card-title ${ this.props.type === 'small_background' && this.props.type === 'small_image' && this.props.type === 'mini' && 'card-title-small'}`}><a href={post.link}>{post.title}</a></div>
