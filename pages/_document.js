7e18620703f65c55e64b6f0a854fbc0c117d50a5
Kumar Abhirup <kumar@iqubex.com>
2019-02-26 19:57:29 +0530

    👌 IMPROVE: Rearrange script tags

--- pages/_document.js
+++ pages/_document.js
@@ -33,14 +33,14 @@ class MyDocument extends Document {
           <NextScript />
 
           <script src="/static/prebuilt/js/jquery-3.2.1.min.js"></script>
-          <script src="/static/prebuilt/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
           <script src="/static/prebuilt/styles/bootstrap4/popper.js"></script>
           <script src="/static/prebuilt/styles/bootstrap4/bootstrap.min.js"></script>
-          <script src="/static/prebuilt/plugins/masonry/masonry.js"></script>
           <script src="/static/prebuilt/plugins/masonry/images_loaded.js"></script>
+          <script src="/static/prebuilt/plugins/masonry/masonry.js"></script>
           <script src="/static/prebuilt/plugins/jquery.mb.YTPlayer-3.1.12/jquery.mb.YTPlayer.js"></script>
           <script src="/static/prebuilt/plugins/easing/easing.js"></script>
           <script src="/static/prebuilt/plugins/parallax-js-master/parallax.min.js"></script>
+          <script src="/static/prebuilt/plugins/OwlCarousel2-2.2.1/owl.carousel.js"></script>
 
           <script src="/static/prebuilt/js/custom.js"></script>
           <script src="/static/prebuilt/js/post.js"></script>
