4f20ab42438db556063ee20734b5a8882b957931
Kumar Abhirup <kumar@iqubex.com>
2019-03-04 13:12:30 +0530

    👌 IMPROVE: Remove useless Login page scripts and styles

--- pages/login.js
+++ pages/login.js
@@ -27,14 +27,6 @@ export default class loginPage extends Component {
           `}}></script>
         </Head>
         <LoginPage />
-        <Head>
-          <script src="/static/auth/vendor/animsition/js/animsition.min.js"></script>
-          <script src="/static/auth/vendor/select2/select2.min.js"></script>
-          <script src="/static/auth/vendor/daterangepicker/moment.min.js"></script>
-          <script src="/static/auth/vendor/daterangepicker/daterangepicker.js"></script>
-          <script src="/static/auth/vendor/countdowntime/countdowntime.js"></script>
-          <script src="/static/auth/js/main.js"></script>
-        </Head>
       </>
     )
   }
