87845ab4cbbc15fb062d07f17c595388d61f0de2
Kumar Abhirup <kumar@iqubex.com>
2019-03-10 11:39:42 +0530

    👌 IMPROVE: Add Profile Dropdown in Desktop header

--- src/components/Header/profileButton.js
+++ src/components/Header/profileButton.js
@@ -1,31 +1,27 @@
 import React, { Component } from 'react'
+import { Dropdown } from 'react-bootstrap'
+
+import Signout from '../Signout'
 
 export default class ProfileButton extends Component {
-
-  state = {
-    dropdown: false
-  }
-
-  popDropdown = async () => {
-    await this.setState({ dropdown: !this.state.dropdown })
-    console.log(this.state.dropdown)
-  }
 
   render() {
     const { me } = this.props
     return (
       <>
-      <li style={{backgroundColor: '#ffffff', color: '#000000', borderRadius: '5px', padding: '5px 10px', zoom: '90%'}}>
-        <span style={{cursor: 'pointer'}}>
+      <Dropdown style={{display: 'inline-block', cursor: 'pointer'}}>
+        <Dropdown.Toggle variant="light" id="dropdown-basic" size="sm">
           <img src={me.profilePicture} width="20px" style={{borderRadius: '100%'}} />
           &nbsp;&nbsp; { me.name }
-        </span> <span onClick={this.popDropdown} style={{cursor: 'pointer'}}>{this.state.dropdown ? '👆' : '👇'}</span>
-      </li>
-      { this.state.dropdown && (
-        <div style={{position: 'absolute', background: '#d3d3d3', padding: '10px', width: '200px'}}>
-          THIS IS ME
-        </div>
-      ) }
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item>Profile</Dropdown.Item>
+          <Dropdown.Item>
+            <Signout />
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
       </>
     )
   }
