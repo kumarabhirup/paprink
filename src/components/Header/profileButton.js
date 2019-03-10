ae8b18511039bacca830c20c90d017b3093aee45
Kumar Abhirup <kumar@iqubex.com>
2019-03-10 10:07:19 +0530

    ❎ SAVE: Add a dropdown state

--- src/components/Header/profileButton.js
+++ src/components/Header/profileButton.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react'
 
 export default class ProfileButton extends Component {
+
+  state = {
+    dropdown: false
+  }
+
+  popDropdown = async () => {
+    await this.setState({ dropdown: !this.state.dropdown })
+    console.log(this.state.dropdown)
+  }
+
   render() {
     const { me } = this.props
     return (
       <>
-      <li style={{backgroundColor: '#ffffff', color: '#000000', borderRadius: '5px', padding: '5px 10px'}}>
+      <li style={{backgroundColor: '#ffffff', color: '#000000', borderRadius: '5px', padding: '5px 10px', zoom: '90%'}}>
         <span style={{cursor: 'pointer'}}>
           <img src={me.profilePicture} width="20px" style={{borderRadius: '100%'}} />
-          &nbsp;&nbsp; { me.name } 
-        </span>
+          &nbsp;&nbsp; { me.name }
+        </span> <span onClick={this.popDropdown} style={{cursor: 'pointer'}}>{this.state.dropdown ? '👆' : '👇'}</span>
       </li>
+      { this.state.dropdown && (
+        <div style={{position: 'absolute', background: '#d3d3d3', padding: '10px', width: '200px'}}>
+          THIS IS ME
+        </div>
+      ) }
       </>
     )
   }
+
 }
