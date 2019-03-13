6ec246e53744fdbc3502fc0eaa8043e8d4aa2353
Kumar Abhirup <kumar@iqubex.com>
2019-03-13 12:18:31 +0530

    👌 IMPROVE: Add selection styling

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react'
 import { Editor, EditorState } from 'draft-js'
 
 export default class PaprinkEditor extends Component {
-
+ 
   constructor(props) {
-    super(props);
+    super(props)
     this.state = {editorState: EditorState.createEmpty()}
     this.onChange = (editorState) => this.setState({editorState})
   }
