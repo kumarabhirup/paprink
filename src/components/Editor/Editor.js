5840f17e22e9dee233ef166b40133fdcaea1f364
Kumar Abhirup <kumar@iqubex.com>
2019-03-14 22:47:40 +0530

    👌 IMPROVE: Add fancy placeholder

--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
-import Dante from 'Dante2'
 import Head from 'next/head'
+import dynamic from 'next/dynamic'
+
+// import Dante from 'Dante2'
+const Dante = dynamic(import('Dante2'), {
+  ssr: false
+})
 
 export default class PaprinkEditor extends Component {
   render() {
     return (
-      <Dante />
+      <Dante
+        content={null}
+        body_placeholder={'Write your next masterpiece ✍️'}
+      />
     )
   }
 }
