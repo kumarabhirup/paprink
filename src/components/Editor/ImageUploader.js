0f597ffc9f14051f8d46a1a782f8b1fbd1dbcd32
Kumar Abhirup <kumar@iqubex.com>
2019-03-17 18:58:31 +0530

    👌 IMPROVE: Write code for thumbnailUpload

--- src/components/Editor/ImageUploader.js
+++ src/components/Editor/ImageUploader.js
@@ -1,6 +1,34 @@
 import React, { Component } from 'react'
 
 export default class ImageUploader extends Component {
+
+  state = {
+    image: null
+  }
+
+  thumbnailUpload = async e => {
+
+    const files = e.target.files
+
+    const data = new FormData()
+    data.append('file', files[0])
+    data.append('upload_preset', 'paprink')
+
+    const res = await fetch(`https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_USERNAME}/image/upload`, {
+      method: 'POST',
+      body: data
+    }).then(res => (res.json()))
+    
+    console.log(res)
+
+    this.setState({
+      image: res.secure_url,
+      smallImage: res.eager[0].secure_url,
+      blackOverlayImage: res.eager[1].secure_url,
+    })
+
+  }
+
   render() {
     return (
       <div className="white-box-avision">
@@ -8,11 +36,12 @@ export default class ImageUploader extends Component {
         <div className="bottom_panel d-flex flex-row align-items-center justify-content-start" style={{padding: '15px 0px'}}>
           <a>❌</a>
           &nbsp;&nbsp;&nbsp;
-          <input type="file" style={{width: '100%'}} />
+          <input type="file" id="thumbnailUpload" name="thumbnailUpload" style={{width: '100%'}} required onChange={this.thumbnailUpload} />
           &nbsp;&nbsp;&nbsp;
           <a>UPLOADING...</a>
         </div>
       </div>
     )
   }
+
 }
