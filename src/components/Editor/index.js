3a0a36877ea32ce847285d5755d995f3759a75f7
Kumar Abhirup <kumar@iqubex.com>
2019-03-27 14:03:03 +0530

    🐛 FIX: fix undefined slug problem

--- src/components/Editor/index.js
+++ src/components/Editor/index.js
@@ -56,6 +56,7 @@ const SAVE_POST_MUTATION = gql`
       thumbnail
       categories
       status
+      slug
     }
   }
 `
@@ -93,6 +94,7 @@ const UPDATE_POST_MUTATION = gql`
       thumbnail
       categories
       status
+      slug
     }
   }
 `
