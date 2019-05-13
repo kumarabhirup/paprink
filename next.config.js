d2bf93e7d4270981903e03a01883602fe788f7fc
Kumar Abhirup <kumar@iqubex.com>
2019-05-13 22:42:02 +0530

    ❎ SAVE: Try as given the issue https://github.com/zeit/next.js/issues/7320

--- next.config.js
+++ next.config.js
@@ -1,11 +1,10 @@
+require('dotenv').config()
 const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass')
-const { parsed: localEnv } = require('dotenv').config()
 const webpack = require('webpack')
 
 module.exports = withSass({
   webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv))
     config.plugins.push(
       new webpack.ProvidePlugin({
           '$': 'jquery',
@@ -13,5 +12,8 @@ module.exports = withSass({
       })
     )
     return config
+  },
+  env: {
+    'PROD_DOMAIN': process.env.PROD_DOMAIN
   }
 })
