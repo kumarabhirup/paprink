b7ce187f27f2d3c8b3ccb43283f616df71c7e95d
Kumar Abhirup <kumar@iqubex.com>
2019-06-01 11:49:19 +0530

    📦 NEW: Use dep remover

---
+++ unused-dep-identifier.sh
@@ -0,0 +1,5 @@
+for dep in $(jq -r '.dependencies | keys | .[]' package.json); do
+ if ! grep "require\(.*$dep.*\)" -Rq --exclude-dir="node_modules" .; then
+  echo "You can probably remove $dep"
+ fi
+done
