fc14a1845f87cd2992c3dcedf84a4cacf97b8670
Kumar Abhirup <kumar@iqubex.com>
2019-06-02 12:18:06 +0530

    👌 IMPROVE: Small Profile button (#41)
    * 👌 IMPROVE: React fast for upvote button click
    It reacts fast for the first time user clicks on Upvote button (optimistic response)
    
    * 👌 IMPROVE: Small Profile button
    So that mobie view doesn't collapse

--- src/components/Header/profileButton.js
+++ src/components/Header/profileButton.js
@@ -1,25 +1,19 @@
 import React, { Component } from 'react'
 import { Dropdown } from 'react-bootstrap'
-import isMobile from 'is-mobile'
 
 import Signout from './Signout'
 
 export default class ProfileButton extends Component {
-
-  toggleStyles = () => {
-    if (isMobile()) return {width: "145px", overflow: "hidden"}
-    else return {width: "auto"}
-  }
 
   render() {
     const { me } = this.props
     return (
       <>
       <Dropdown style={{display: 'inline-block', cursor: 'pointer'}}>
-        <Dropdown.Toggle variant="light" id="dropdown-basic" size="sm" style={this.toggleStyles()}>
+        <Dropdown.Toggle variant="light" id="dropdown-basic" size="sm" style={{width: "auto"}}>
           <img src={me.profilePicture} width="20px" style={{borderRadius: '100%'}} />
           &nbsp;&nbsp; 
-          { me.name }
+          { me.name.substr(0, 5) }..
         </Dropdown.Toggle>
 
         <Dropdown.Menu>
